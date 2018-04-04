0a1423e3886320e90f8bad003a91f6baa6633eb2
Leon <leon-lukas11@web.de>
2018-04-04 07:11:39 +0200

    style

--- src/edu/hm/llukas/prae/CommentOut.java
+++ src/edu/hm/llukas/prae/CommentOut.java
@@ -5,56 +5,57 @@ import edu.hm.cs.rs.compiler.toys.base.Preprocessor;
 import edu.hm.cs.rs.compiler.toys.base.Source;
 
 public class CommentOut implements Preprocessor {
-    Source source;
+    private final Integer zero = 0;
+    private final Integer one = 1;
+    private final Integer two = 2;
+    private final Integer three = 3;
+    private final Integer four = 4;
+
 
     @Override
     public Source process(Source source) throws LexicalError {
-        Source result = new Source();
-        int state = 0;
+        final Source result = new Source();
+        int state = zero;
         while (source.hasMore()) {
-            char c = source.getNextChar();
-            if (c == '/' && state == 0) {
-                state = 1;
-            }else if(c == '/' && state == 1){
-                state = 2;
-            }else if(c == '*' && state == 1 ){
-                state = 3;
-            }else if(c != '/' && state == 1){
+            final char c = source.getNextChar();
+            if (c == '/' && state == zero) {
+                state = one;
+            } else if (c == '/' && state == one) {
+                state = two;
+            } else if (c == '*' && state == one) {
+                state = three;
+            } else if (c != '/' && state == one) {
                 result.append('/');
                 result.append(c);
-                state = 0;
-            }else if(state == 2){
-                if(c == '\n'){
-                    state = 0;
+                state = zero;
+            } else if (state == two) {
+                if (c == '\n') {
+                    state = zero;
                     result.append('\n');
                 }
-            }else if(c == '\n' && state == 3) {
+            } else if (c == '\n' && state == three) {
                 result.append(c);
-            }
-            else if(c == '*' && state == 3) {
-                state = 4;
-            }else if(state == 3){}
-            else if(c == '/' && state == 4) {
-                state = 0;
+            } else if (c == '*' && state == three) {
+                state = four;
+            } else if (state == three) {
+            } else if (c == '\n' && state == four) {
+                result.append(c);
+            } else if (c == '/' && state == four) {
+                state = zero;
                 result.append(' ');
-            }else if (state == 4){
-                if(c == '*'){
-
-
-                }else {
-                    state = 3;
-                }
-            }
-            else  {
+            } else if (state == four && c != '*') {
+                state = three;
+            } else if (state == four && c == '*') {
+            } else {
                 result.append(c);
             }
 
         }
-        if(state == 4 || state == 3){
-            throw new LexicalError();
-        }
-        if(state == 1){
+        if (state == one) {
             result.append('/');
+        }
+        if (state == four || state == three || state == two) {
+            throw new LexicalError();
         }
         return result;
     }
