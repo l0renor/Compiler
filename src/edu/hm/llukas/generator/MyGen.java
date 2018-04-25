90cc47d104728ae8a24cfee4f9ecd659e074544b
hm-Llukas <leon-lukas11@web.de>
2018-04-25 11:21:28 +0200

    done

--- src/edu/hm/llukas/generator/MyGen.java
+++ src/edu/hm/llukas/generator/MyGen.java
@@ -1,44 +1,84 @@
 package edu.hm.llukas.generator;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collector;
+import edu.hm.cs.rs.compiler.lab04generator.LanguageGenerator;
+
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class MyGen implements LanguageGenerator{
+
+public class MyGen implements LanguageGenerator {
     @Override
-    public Stream<String> generate(Stream<String[]> grammar, int uptoLength) {
+    public Stream<String> generate(Stream<String[]> grammar, int uptoLengthint) {
+        long uptoLength = uptoLengthint;
         List<String[]> gram = grammar.collect(Collectors.toList());
         List<String> words = new ArrayList<>();
-        words.add(gram.get(0)[0]);
-        while (true) {
-            boolean finished = true;
-            for (int i = 0;i<words.size();i++) {
+        List<String> unfinischedWords = new ArrayList<>();
+        unfinischedWords.add(gram.get(0)[0]);
+        boolean finished = false;
+        while (!finished) {
+            finished = true;
+            List<String> tmpList = new ArrayList<>();
+
+            for (int i = 0; i < unfinischedWords.size(); i++) {
                 for (String[] rule : gram) {
-                    if (words.get(i).contains(rule[0])) {
-                        words.add(words.get(i).replaceFirst(rule[0], rule[1]));
+                    if (unfinischedWords.get(i).contains(rule[0])) {
+                        String tmp = unfinischedWords.get(i).replaceFirst(rule[0], rule[1]);
+                        if (tmp.toLowerCase().equals(tmp)) {
+                            if (tmp.length() <= uptoLength) {
+
+                                words.add(tmp);
+                            }
+                        } else {
+                            if (tmp.length() <= uptoLength) {
+                                tmpList.add(tmp);
+                            }
+                        }
+
                     }
                 }
             }
-            for (String word: words) {
-                if (word.length() < uptoLength && word.equals(word.toLowerCase())){
-                    finished = false;
-                }
-            }
-            if (finished) {break;}
+
+            unfinischedWords = new ArrayList<>(new HashSet<>(tmpList));
+            finished = unfinischedWords.isEmpty();
         }
-        return words.stream();
+        words = new ArrayList<>(new HashSet<>(words));
+        return words.stream().distinct().sorted(
+                Comparator.comparingInt(String::length)
+                        .thenComparing(Comparator.naturalOrder())
+        );
+    }
+
+    private boolean finished(List<String> list, long len) {
+        for (String word : list) {
+            if (word.length() < len + 2) {
+                return false;
+            }
+
+        }
+
+        return true;
     }
 
     @Override
     public Stream<String[]> parseGrammar(String grammarString) {
-        return Stream.of(grammarString.split(String.valueOf(grammarString.charAt(1)))).skip(1).map(s -> s.split((String.valueOf(grammarString.charAt(0)))));
+        return Stream.of(grammarString.split(String.valueOf(grammarString.charAt(1)))).skip(1).map(s -> s.split((String.valueOf(grammarString.charAt(0))))).map(strings -> {
+            if (strings.length == 1) {
+                return new String[]{strings[0], ""};
+            } else {
+                return strings;
+            }
+        });
     }
-    public static void main(String [] args){
-        //System.out.println(new MyGen().parseGrammar("=;S=();S=(S);S=()S;S=(S)S"));
-        new MyGen().parseGrammar("=;S=();S=(S);S=()S;S=(S)S").forEach(strings -> System.out.println(strings[0]+"->"+strings[1]));
-        new MyGen().generate(new MyGen().parseGrammar("=;S=();S=(S);S=()S;S=(S)S"),5).forEach(s -> System.out.println(s));
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        //System.out.println("HALLo".contains("[A-Z]"));
+       // new MyGen().parseGrammar("=;S=();S=(S);S=()S;S=(S)S").forEach(strings -> System.out.println(strings[0] + "->" + strings[1]));
+        new MyGen().generate(new MyGen().parseGrammar("=,S=,S=X,X=b,X=a,X=bX,X=aY,Y=aZ,Z=aZ,Z=bbL,Z=bbX,L=aL,L=bM,M=b,M=bM"), 5).forEach(
+                (x)-> System.out.println(x));
+        System.out.println(start-System.currentTimeMillis());
 
     }
 }
+//6 =,S=,S=X,X=b,X=a,X=bX,X=aY,Y=aZ,Z=aZ,Z=bbL,Z=bbX,L=aL,L=bM,M=b,M=bM
